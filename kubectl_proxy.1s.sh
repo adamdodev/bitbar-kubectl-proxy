7c88759fe2fbf5b40061b9d6e456f3bc060ade33
Adam Westbrook <snap6sic6@gmail.com>
2017-07-05 11:58:05 +0100

    Create kubectl_proxy.1s.sh

---
+++ kubectl_proxy.1s.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# kubectl proxy control and context switcher for bitbar
+# @author Adam Westbrook <adam@adamwestbrook.info>
+# @requires kubectl, zsh
+
+# Make sure kubectl is explicitly in your PATH, e.g.
+#export PATH=~/google-cloud-sdk/bin:"$PATH"
+PID="$(pgrep -f "kubectl proxy")"
+CONTEXT="$(kubectl config current-context)"
+CONTEXTS="$(kubectl config get-contexts --output=name)"
+
+if [ ! -z "$PID" ]; then
+  echo "kubectl proxy RUNNING"
+  echo "---"
+  echo $CONTEXT
+  echo "Stop | bash='kill -9 "$PID" && exit' terminal=true"
+  echo "Switch Context (Proxy must be stopped)"
+else
+  echo "kubectl proxy STOPPED"
+  echo "---"
+  echo $CONTEXT
+  echo "Start | bash='nohup /Users/AdamWestbrook/Misc/google-cloud-sdk/bin/kubectl proxy &! exit' terminal=true"
+  echo "Switch Context"
+  for i in $CONTEXTS; do
+  	echo "-- $i | bash='/Users/AdamWestbrook/Misc/google-cloud-sdk/bin/kubectl config use-context $i && exit' terminal=true"
+  done
+fi
